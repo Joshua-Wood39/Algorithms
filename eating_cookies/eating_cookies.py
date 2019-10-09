cd562f2bf33d83911bdc70827698f8b444bad636
Joshua Wood <JerichoWall@apu.edu>
2019-10-09 15:43:58 -0700

    Broke down one idea, added an options; not sure how to implement this...

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,14 +3,33 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
+
+
 def eating_cookies(n, cache=None):
-  pass
+    options = [1, 2, 3]
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+            ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
+
+
+# 11111,
+# 1211,
+# 1121,
+# 2111,
+# 221,
+# 212,
+# 122,
+# 1112,
+# 32,
+# 311,
+# 131,
+# 23,
+# 113
