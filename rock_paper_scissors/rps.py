8f7453e04c4d74f77c17f1f5642962c3cc2495b3
Joshua Wood <JerichoWall@apu.edu>
2019-10-10 16:17:22 -0700

    Finished rps - was totally overcomplicating things

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,28 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    core = ["rock", "paper", "scissors"]
+    results = []
+
+    def looper(cycles, temp_result=[]):
+
+        if cycles < 1:
+            print(f"temp: {temp_result}")
+            return results.append(temp_result)
+        else:
+            for v in core:
+                print(f"v: {v}")
+                looper(cycles - 1, temp_result + [v])
+
+    looper(n)
+    return results
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
